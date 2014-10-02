882dc2e25aa4b360cea061f19c89e08d9022bb03
hc3937 <hc3937@mct165s16.cs.ship.edu>
2014-10-02 12:10:05 -0400

    Added Friend Mapper

--- swe400fitness/src/mapper/FriendMapper.java
+++ swe400fitness/src/mapper/FriendMapper.java
@@ -1,20 +1,51 @@
 package mapper;
 
-import domainModel.DomainObject;
+import gateway.FriendGateway;
+import domainModel.FriendList;
 
-public class FriendMapper implements Mapper
+/**
+ * 
+ * @author Hayden Cook
+ *
+ */
+public class FriendMapper 
 {
-
-	@Override
-	public void insert(DomainObject o)
+	FriendGateway friendGate;
+	
+	public FriendList find(long id)
 	{
-		
+		RecordSet record = friendGate.find(id);
+		FriendList friend = load(record);
+		return friend;
 	}
-
-	@Override
-	public void update(DomainObject o)
+	
+	public FriendList load(RecordSet record)
 	{
-		
+		String user = record.getString("user_name");
+		int friendID = record.getInt("friendID");
+		FriendList friend = new FriendList(user, friendID);
+		return friend;
 	}
-
+	
+	public void update(FriendList friend)
+	{
+		String user = friend.getUser();
+		int friendID = friend.getID();
+		friendGate.update(user, friendID);
+	}
+	
+	public void create(FriendList friend)
+	{
+		String user = friend.getUser();
+		int friendID = friend.getID();
+		friendGate.create(user, friendID);
+	}
+	
+	public void delete(FriendList friend)
+	{
+		int friendID = friend.getID();
+		friendGate.delete(friendID);
+	}
+	
+	
 }
