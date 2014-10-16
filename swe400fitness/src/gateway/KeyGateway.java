3ff9c57fedd0a135862f7c2d8944d43e9a4bfe04
funcvehicle <skagboymilkminus@gmail.com>
2014-10-16 10:58:00 -0400

    Completed KeyGateway

--- swe400fitness/src/gateway/KeyGateway.java
+++ swe400fitness/src/gateway/KeyGateway.java
@@ -1,9 +1,43 @@
 package gateway;
 
-public class KeyGateway 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class KeyGateway extends Gateway
 {
+	
+	private void incrementKey(Connection connection,long current)
+	{
+		try {
+
+			Statement statement = connection.createStatement();
+			statement.execute("UPDATE keytable SET key="+(current+1)+" WHERE key="+current);
+		} catch (SQLException e) {
+			System.err.println("Couldn't get the key!");
+		}
+	}
+	private long getCurrentKey(Connection connection)
+	{
+		Statement statement;
+		try {
+			
+			statement = connection.createStatement();
+			ResultSet results = statement.executeQuery("SELECT * FROM keytable");
+			long returnMe = results.getLong("key");
+			incrementKey(connection, returnMe);
+			return returnMe;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return -1;
+	}
 	public long generateKey()
 	{
-		return 0;
+		establishConnection();
+		Connection connection = getConnection();
+		return getCurrentKey(connection);
 	}
 }
